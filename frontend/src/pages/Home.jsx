cbdb5a845e9b3f82802e618431c3de1c592a4c5c
samuelsenerwa <samuelsenerwa@gmail.com>
2023-11-13 21:00:45 +0300

    Home section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,6 +4,8 @@ import { Container, Row, Col } from "reactstrap";
 import heroImg from "../assets/images/hero-img01.jpg";
 import heroImg02 from "../assets/images/hero-img02.jpg";
 import heroVideo from "../assets/images/hero-video.mp4";
+import worldImg from "../assets/images/world.png";
+import Subtitle from "../shared/Subtitle";
 
 const Home = () => {
   return (
@@ -14,7 +16,35 @@ const Home = () => {
           <Row>
             <Col lg="6">
               <div className="hero__content">
-                <div className="hero__subtitle d-flex align-items-center"></div>
+                <div className="hero__subtitle d-flex align-items-center">
+                  <Subtitle subtitle={"Know Before You Go"} />
+                  <img src={worldImg} alt="" />
+                </div>
+                <h1>
+                  Travelling opens the door to creating
+                  <span className="highlight"> memories</span>
+                </h1>
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam
+                  magnam maxime libero, quae dolor provident, a, blanditiis
+                  numquam dignissimos velit recusandae officia quasi omnis
+                  temporibus deserunt inventore itaque. Harum, ipsam?
+                </p>
+              </div>
+            </Col>
+            <Col lg="2">
+              <div className="hero__img-box">
+                <img src={heroImg} alt="" />
+              </div>
+            </Col>
+            <Col lg="2">
+              <div className="hero__img-box">
+                <video src={heroVideo} alt="" controls />
+              </div>
+            </Col>
+            <Col lg="2">
+              <div className="hero__img-box">
+                <img src={heroImg02} alt="" />
               </div>
             </Col>
           </Row>
