1b4efa023a51c833caa3f4990ab988dd3daf6b4b
samuelsenerwa <samuelsenerwa@gmail.com>
2023-12-07 12:12:50 +0300

    feat: added search functionality in hero section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,6 +6,7 @@ import heroImg02 from "../assets/images/hero-img02.jpg";
 import heroVideo from "../assets/images/hero-video.mp4";
 import worldImg from "../assets/images/world.png";
 import Subtitle from "../shared/Subtitle";
+import SearchBar from "../shared/SearchBar";
 
 const Home = () => {
   return (
@@ -47,6 +48,8 @@ const Home = () => {
                 <img src={heroImg02} alt="" />
               </div>
             </Col>
+
+            <SearchBar/>
           </Row>
         </Container>
       </section>
--- frontend/src/shared/SearchBar.jsx
+++ frontend/src/shared/SearchBar.jsx
@@ -5,7 +5,7 @@ import { Col, Form, FormGroup } from "reactstrap";
 const SearchBar = () => {
   return (
     <Col lg="12">
-      <div className="search_bar">
+      <div className="search__bar">
         <form className="d-flex align-items-center gap-4">
           <FormGroup className="d-flex gap-3 form__group form__group-fast">
             <span>
@@ -18,19 +18,19 @@ const SearchBar = () => {
           </FormGroup>
           <FormGroup className="d-flex gap-3 form__group form__group-fast">
             <span>
-              <i class="ri-map-pin-line"></i>
+            <i class="ri-roadster-line"></i>
               <div>
-                <h6>Location</h6>
-                <input type="text" placeholder="Where are you going?" />
+                <h6>Distance</h6>
+                <input type="number" placeholder="Distance in km/h" />
               </div>
             </span>
           </FormGroup>
-          <FormGroup className="d-flex gap-3 form__group form__group-fast">
+          <FormGroup className="d-flex gap-3 form__group form__group-last">
             <span>
-              <i class="ri-map-pin-line"></i>
+            <i class="ri-group-line"></i>
               <div>
-                <h6>Location</h6>
-                <input type="text" placeholder="Where are you going?" />
+                <h6>Max people</h6>
+                <input type="number" placeholder="0" />
               </div>
             </span>
           </FormGroup>
