60292dd5ab8d2b29ba23ee6143cf91712443c485
samuelsenerwa <samuelsenerwa@gmail.com>
2023-12-16 20:36:13 +0300

    feat: imported components for the featured, searchbar and TourDetails sections

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,6 +7,8 @@ import heroVideo from "../assets/images/hero-video.mp4";
 import worldImg from "../assets/images/world.png";
 import Subtitle from "../shared/Subtitle";
 import SearchBar from "../shared/SearchBar";
+import ServiceList from "../services/ServiceList";
+import FeaturedTourList from "../components/Featured-tours/FeaturedTourList";
 
 const Home = () => {
   return (
@@ -49,12 +51,38 @@ const Home = () => {
               </div>
             </Col>
 
-            <SearchBar/>
+            <SearchBar />
           </Row>
         </Container>
       </section>
 
       {/* =========== hero section ends ============ */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg="3">
+              <h5 className="services__subtitle">What we serve</h5>
+              <h2 className="services__title">We offer our best services</h2>
+            </Col>
+            <ServiceList />
+          </Row>
+        </Container>
+      </section>
+
+      {/* ============== featured tour section start */}
+
+        <section>
+          <Container>
+            <Row>
+              <Col lg="12" className="mb-5">
+                <Subtitle subtitle={'Explore'}/>
+                <h2 className="featured__tour-title">Our featured tours</h2>
+              </Col>
+              <FeaturedTourList />
+            </Row>
+          </Container>
+        </section>
+      {/* ============== featured tour section end */}
     </>
   );
 };
