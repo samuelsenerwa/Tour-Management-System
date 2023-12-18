c22ac9ee8ae3b2160af4bc4e4d998c4d440062d6
samuelsenerwa <samuelsenerwa@gmail.com>
2023-12-18 22:48:30 +0300

    feat: added tours, testimonial, newsletter, image-gallery components

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,16 +5,21 @@ import heroImg from "../assets/images/hero-img01.jpg";
 import heroImg02 from "../assets/images/hero-img02.jpg";
 import heroVideo from "../assets/images/hero-video.mp4";
 import worldImg from "../assets/images/world.png";
+import experienceImg from '../assets/images/experience.png'
 import Subtitle from "../shared/Subtitle";
 import SearchBar from "../shared/SearchBar";
 import ServiceList from "../services/ServiceList";
 import FeaturedTourList from "../components/Featured-tours/FeaturedTourList";
+import MasonryImageGallery from "../components/image-gallery/MasonryImageGallery";
+import Testimonial from "../components/Testimonial/Testimonial";
+import Newsletter from "../shared/Newsletter";
+
 
 const Home = () => {
   return (
     <>
       {/* ============ hero section start =========== */}
-      <section>
+      <section className="pb-0">
         <Container>
           <Row>
             <Col lg="6">
@@ -83,6 +88,86 @@ const Home = () => {
           </Container>
         </section>
       {/* ============== featured tour section end */}
+
+      {/* ============== experience section start ============= */}
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg='6'>
+              <div className="experience__content">
+                <Subtitle subtitle={'Experience'}/>
+                <h2>With our all experience <br /> We will serve you</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  <br />
+                 Et necessitatibus qui vel soluta numquam temporibus, quos veritatis quod rem provident quaerat eaque distinctio eum quis a, harum, aliquid eos earum.
+                </p>
+              </div>
+
+              <div className="counter__wrapper d-flex align-items-center gap-5">
+                <div className="counter__box">
+                  <span>12k+ </span>
+                  <h6>Successful trips</h6>
+                </div>
+                <div className="counter__box">
+                  <span>2k+ </span>
+                  <h6>Regular clients</h6>
+                </div>
+                <div className="counter__box">
+                  <span>10 </span>
+                  <h6>Years of experience</h6>
+                </div>
+              </div>
+            </Col>
+            <Col lg='6'>
+              <div className="experience__img">
+                <img src={experienceImg} alt="" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      {/* ============== experience section ends ============= */}
+
+      {/* ============== gallery section start =============== */}
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg='12'>
+              <Subtitle subtitle={'Gallery'}/>
+              <h2 className="gallery_title">Take a look at our tour gallery</h2>
+            </Col>
+            <Col lg='12'>
+              <MasonryImageGallery/>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      {/* ============== gallery section ends =============== */}
+
+      {/*================ testimonials section starts ============ */}
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12">
+              <Subtitle subtitle={'Fans Love'}/>
+              <h2 className="testimonial__title">What our fans say about us</h2>
+            </Col>
+            <Col lg='12'>
+              <Testimonial/>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      {/*================ testimonials section ends ============ */}
+
+      <Newsletter />
     </>
   );
 };
