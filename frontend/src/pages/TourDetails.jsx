4a6e95422f2677a10881f6ac3a8bc7b1e6665f3d
samuelsenerwa <samuelsenerwa@gmail.com>
2023-12-20 21:29:39 +0300

    feat: implemented tour booking functionality

--- frontend/src/pages/TourDetails.jsx
+++ frontend/src/pages/TourDetails.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "../styles/tour-details.css";
 import { Container, Row, Col, Form, ListGroup } from "reactstrap";
 import { useParams } from "react-router-dom";
 import tourData from "../assets/data/tours";
 import calculateAvgRating from "../utils/avgRating";
+import avatar from "../assets/images/avatar.jpg";
+import Booking from "../components/Booking/Booking";
+import Newsletter from "../shared/Newsletter";
 
 const TourDetails = () => {
   const { id } = useParams();
+  const reviewMsgRef = useRef("");
+  const [tourRating, setTourRating] = useState(null);
 
   // static data for testing before using one from the DB
   const tour = tourData.find((tour) => tour.id === id);
@@ -26,6 +31,23 @@ const TourDetails = () => {
 
   const { totalRating, avgRating } = calculateAvgRating(reviews);
 
+  // format date
+  const options = {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  };
+
+  // submit request to the server
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const reviewText = reviewMsgRef.current.value;
+
+    // alert(`${reviewText}, ${tourRating}`)
+
+    // handle calls from the api
+  };
+
   return (
     <>
       <section>
@@ -37,12 +59,12 @@ const TourDetails = () => {
                 <div className="tour__info">
                   <h2>{title}</h2>
                   <div className="d-flex align-items-center gap-5">
-                    <span className="tour__review d-flex align-items-center gap-1">
+                    <span className="tour__rating d-flex align-items-center gap-1">
                       <i
                         class="ri-star-fill"
                         style={{ color: "var(--secondary-color)" }}
                       ></i>
-                      {calculateAvgRating === 0 ? null : avgRating}
+                      {avgRating === 0 ? null : avgRating}
                       {totalRating === 0 ? (
                         "Not rated"
                       ) : (
@@ -56,24 +78,99 @@ const TourDetails = () => {
                   </div>
                   <div className="tour__extra-details">
                     <span>
-                      <i class="ri-map-pin-fill"></i>
+                      <i class="ri-map-pin-line"></i>
                       {city}
                     </span>
                     <span>
-                      <i class="ri-money-dollar-circle-line"></i>
-                      ${price} / per person
+                      <i class="ri-money-dollar-circle-line"></i>${price} / per
+                      person
                     </span>
                     <span>
-                      <i class="ri-group-line">{maxGroupSize}</i>
-                      {city}
+                      <i class="ri-map-pin-time-line"></i>
+                      {distance} km
+                    </span>
+                    <span>
+                      <i class="ri-group-line">{maxGroupSize} people</i>
                     </span>
                   </div>
+                  <h5>Description</h5>
+                  <p>{desc}</p>
                 </div>
+
+                {/* ==================tour reviews section ========== */}
+                <div className="tour__reviews mt-4">
+                  <h4>Reviews ({reviews?.length} reviews)</h4>
+
+                  <Form onSubmit={submitHandler}>
+                    <div className="d-flex align-items-center gap-3 mb-4 rating__group">
+                      <span onClick={() => setTourRating(1)}>
+                        1<i class="ri-star-s-fill"></i>
+                      </span>
+                      <span onClick={() => setTourRating(2)}>
+                        2<i class="ri-star-s-fill"></i>
+                      </span>
+                      <span onClick={() => setTourRating(3)}>
+                        3<i class="ri-star-s-fill"></i>
+                      </span>
+                      <span onClick={() => setTourRating(4)}>
+                        4<i class="ri-star-s-fill"></i>
+                      </span>
+                      <span onClick={() => setTourRating(5)}>
+                        5<i class="ri-star-s-fill"></i>
+                      </span>
+                    </div>
+                    <div className="review__input">
+                      <input
+                        type="text"
+                        ref={reviewMsgRef}
+                        placeholder="share your views"
+                        required
+                      />
+                      <button
+                        className="btn primary__btn text-white"
+                        type="submit"
+                      >
+                        Submit
+                      </button>
+                    </div>
+                  </Form>
+
+                  <ListGroup className="user__reviews">
+                    {reviews?.map((review) => (
+                      <div className="review__item">
+                        <img src={avatar} alt="" />
+                        <div className="w-100">
+                          <div className="d-flex align-items-center justify-content-between">
+                            <div>
+                              <h5>Alphons mukhwana</h5>
+                              <p>
+                                {new Date("01-12-2023").toLocaleDateString(
+                                  "en-US",
+                                  options
+                                )}
+                              </p>
+                            </div>
+                            <span className="d-flex align-items-center">
+                              5<i class="ri-star-s-fill"></i>
+                            </span>
+                          </div>
+                          <h6>Tour website has offered amazing tours </h6>
+                        </div>
+                      </div>
+                    ))}
+                  </ListGroup>
+                </div>
+                {/* ==================tour reviews section ends ========== */}
               </div>
+            </Col>
+
+            <Col lg="4">
+              <Booking tour={tour} avgRating={avgRating} />
             </Col>
           </Row>
         </Container>
       </section>
+      <Newsletter />
     </>
   );
 };
