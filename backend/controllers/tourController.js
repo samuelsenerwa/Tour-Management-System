3fdcfa484053d9452e9948e9956a7f0413dabe02
samuelsenerwa <samuelsenerwa@gmail.com>
2023-12-22 11:08:32 +0300

    feat: added getSingleTour route and update route

--- backend/controllers/tourController.js
+++ backend/controllers/tourController.js
@@ -7,19 +7,83 @@ export const createTour = async (req, res) => {
   try {
     const savedTour = await newTour.save();
 
-    res
-      .status(200)
-      .json({
-        success: true,
-        message: "Successfully created a new tour",
-        data: savedTour,
-      });
+    res.status(200).json({
+      success: true,
+      message: "Successfully created a new tour",
+      data: savedTour,
+    });
   } catch (err) {
-    res
-    .status(500)
-    .json({
+    res.status(500).json({
       success: false,
       message: "Sorry! Tour couldn't be created",
     });
   }
+};
+
+// update tour
+export const updateTour = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const updatedTour = await Tour.findByIdAndUpdate(
+      id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Successfully updated",
+      data: updatedTour,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Tour failed to be updated!",
+    });
+  }
+};
+
+// delete tour
+export const deleteTour = async (req, res) => {
+  const id = req.params.id;
+  try {
+    await Tour.findByIdAndDelete(id);
+
+    res.status(200).json({
+      success: true,
+      message: "Successfully deleted",
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Tour failed to be deleted!",
+    });
+  }
+};
+
+// getSingle tour
+export const getSingleTour = async (req, res) => {
+    const id = req.params.id;
+    try {
+      const tour = await Tour.findById(id);
+  
+      res.status(200).json({
+        success: true,
+        message: "Tour Found",
+        data: tour
+      });
+    } catch (err) {
+      res.status(400).json({
+        success: false,
+        message: "Not Found!",
+      });
+    }
+};
+
+// getAll tour
+export const getAllTour = async (req, res) => {
+  try {
+  } catch (err) {}
 };
--- backend/routes/tours.js
+++ backend/routes/tours.js
@@ -1,10 +1,22 @@
 import express from 'express'
-import { createTour } from '../controllers/tourController.js';
+import { createTour, updateTour,deleteTour, getSingleTour, getAllTour } from '../controllers/tourController.js';
 
 const router =  express.Router();
 
 // create new tour
 router.post('/', createTour)
+
+// update tour
+router.put('/:id', updateTour)
+
+// delete tour
+router.delete('/:id', deleteTour)
+
+// get single tour
+router.get('/:id', getSingleTour)
+
+// getAlltour
+router.get('/', getAllTour)
 
 
 export default router;
