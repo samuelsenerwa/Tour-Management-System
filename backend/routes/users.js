264792d23a195e8c2990f62a662e9a7766742019
samuelsenerwa <samuelsenerwa@gmail.com>
2023-12-22 23:34:16 +0300

    feat: implemented user route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,18 +1,25 @@
-import express from 'express'
-import { deleteUser, getAllUser, getSingleUser, updateUser } from '../controllers/userController';
+import express from "express";
+import {
+  deleteUser,
+  getAllUser,
+  getSingleUser,
+  updateUser,
+} from "../controllers/userController.js";
 
-const router = express.Router()
+import { verifyAdmin, verifyUser } from "../utils/verifyToken.js";
+
+const router = express.Router();
 
 // update user
 router.put("/:id", updateUser);
 
 // delete user
-router.delete("/:id", deleteUser);
+router.delete("/:id", verifyUser, deleteUser);
 
 // get single user
-router.get("/:id", getSingleUser);
+router.get("/:id", verifyUser, getSingleUser);
 
 // get all users
-router.get("/", getAllUser);
+router.get("/",verifyAdmin, getAllUser);
 
-export default router
+export default router;
