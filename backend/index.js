d03461a1d3f2414329859e15866560a48d6fed31
samuelsenerwa <samuelsenerwa@gmail.com>
2023-12-22 10:09:52 +0300

    feat: implemented create-tour route for the api

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import tourRoute from './routes/tours.js'
 
 dotenv.config();
 
@@ -10,17 +11,30 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 
-// for testing purposes
-app.get("/", (req, res) => {
-  res.send("api is live");
-});
+// database connection
+mongoose.set('strictQuery', false);
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connection successful");
+  } catch (err) {
+    console.log("MongoDb failed to connect");
+  }
+};
 
 // middleware for my routes
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
+// routes
+app.use('/tours', tourRoute)
+
 // checking if nodemon is working correctlyy
 app.listen(port, () => {
+    connect();
   console.log("Server listening on port", port);
 });
