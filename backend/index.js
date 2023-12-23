213eef02181cf82204ec07c069ac70c657894033
samuelsenerwa <samuelsenerwa@gmail.com>
2023-12-23 18:58:36 +0300

    feat:enabled cors option, added booking & review route

--- backend/index.js
+++ backend/index.js
@@ -3,17 +3,24 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import tourRoute from './routes/tours.js'
-import userRoute from './routes/users.js'
+import tourRoute from "./routes/tours.js";
+import userRoute from "./routes/users.js";
+import authRoute from "./routes/auth.js";
+import reviewRoute from "./routes/reviews.js";
+import bookingRoute from './routes/booking.js'
 
 dotenv.config();
 
 // intialize express app
 const app = express();
 const port = process.env.PORT || 8000;
+const corsOptions = {
+  origin: true,
+  credentials: true,
+};
 
 // database connection
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
@@ -28,15 +35,18 @@ const connect = async () => {
 
 // middleware for my routes
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(cookieParser());
 
 // routes
-app.use('/tours', tourRoute)
-app.use('/users', userRoute)
+app.use("/api/v1/tours", tourRoute);
+app.use("/api/v1/users", userRoute);
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/reviews", reviewRoute);
+app.use("/api/v1/booking", bookingRoute)
 
 // checking if nodemon is working correctlyy
 app.listen(port, () => {
-    connect();
+  connect();
   console.log("Server listening on port", port);
 });
